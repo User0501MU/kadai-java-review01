aa554ca8718aeb6493ead48bfe0f868c297a45a8
User0501MU <m-utsunomiya@fores.jp>
2024-05-10 21:54:59 +0900

    プロ野球チームの勝ち負け数を管理しよう

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,34 +1,70 @@
 package baseball;
 
 public class BaseBallTeam {
-    //フィールド
-    String name;
-    int win;//勝ち
-    int lose;//負け
-    int draw;//引き分け
+    // フィールド
+    // private：同じクラス内からのみアクセス可能
+    private String name;// 名前
+    private int win;// 勝ち
+    private int lose;// 負け
+    private int draw;// 引き分け
 
-    //コンストラクタ
-public BaseBallTeam(String name, int win, int lose, int draw) {
-    System.out.println();
-        this.name =name;
-        this.win =win;
-        this.lose =lose;
-        this.draw =draw;
+    // コンストラクタ（引数あり）
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
     }
+
+//1カプセル化　private にしてデータを守り、そのかわりにフィールドにアクセスするためのメソッドであるgetterやsetterを定義する
+    public String getName() {
+        return name;
+    }
+
+    public void setNAme(String name) {
+        this.name = name;// 代入式左辺に「this.」
+    }
+
+//2カプセル化　private にしてデータを守り、そのかわりにフィールドにアクセスするためのメソッドであるgetterやsetterを定義する
+    public String getWin() {
+        return name;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+//3カプセル化　private にしてデータを守り、そのかわりにフィールドにアクセスするためのメソッドであるgetterやsetterを定義する
+    public String getLose() {
+        return name;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+//4カプセル化　private にしてデータを守り、そのかわりにフィールドにアクセスするためのメソッドであるgetterやsetterを定義する
+    public String getBraw() {
+        return name;
+    }
+
+    public void setBraw(int draw) {
+        this.draw = draw;
+    }
+
+
+
+
 
 // 勝率を計算するメソッド
-public double getRate() {
-    return (double) win / (win + lose);//計算結果を小数値（double）で戻り値
-}
+    public double getRate() {// getter
+        return (double) win / (win + lose);// 計算結果を小数値（double）で戻り値
+    }
 
 // レポートを表示するメソッド
-public void report() {
-    System.out.println(name + "の2022年の成績は " + win + "勝 " + lose + "敗 " + draw + "分、勝率は " + getRate());
+    public void report() {
+        System.out.println(name + "の2022年の成績は " + win + "勝 " + lose + "敗 " + draw + "分、勝率は " + getRate());
+        // getRate;勝率 フィールドの勝ち数や負け数の値を利用して勝率を計算、戻り値として返すという処理をするメソッド
 
-
-}
     }
-//getRate;勝率 フィールドの勝ち数や負け数の値を利用して勝率を計算、戻り値として返すという処理をするメソッド
-
-
-
+}
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -33,25 +33,23 @@ package baseball;
 public class Review03 {
 //メソッド
     public static void main(String[] args) {
-        //それぞれのチームの名前、勝ち数、負け数、引き分け数
-        //変数任意　
-        BaseBallTeam  A1= new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);//東京ヤクルトスワローズ
-        BaseBallTeam  A2= new BaseBallTeam("横浜DeNAベイスターズ",73,68,2);//横浜DeNAベイスターズ
-        BaseBallTeam  A3= new BaseBallTeam("阪神タイガース",68,71,4);//阪神タイガース
-        BaseBallTeam  A4= new BaseBallTeam("読売ジャイアンツ",68,72,3);//読売ジャイアンツ
-        BaseBallTeam  A5= new BaseBallTeam("広島東洋カープ",66,74,3);//広島東洋カープ
-        BaseBallTeam  A6= new BaseBallTeam("中日ドラゴンズ",66,75,2);//中日ドラゴンズ
+        // それぞれのチームの名前、勝ち数、負け数、引き分け数
+        // 変数任意
+        BaseBallTeam a1 = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);// 東京ヤクルトスワローズ
+        BaseBallTeam a2 = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);// 横浜DeNAベイスターズ
+        BaseBallTeam a3 = new BaseBallTeam("阪神タイガース", 68, 71, 4);// 阪神タイガース
+        BaseBallTeam a4 = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);// 読売ジャイアンツ
+        BaseBallTeam a5 = new BaseBallTeam("広島東洋カープ", 66, 74, 3);// 広島東洋カープ
+        BaseBallTeam a6 = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);// 中日ドラゴンズ
 
-    //試合勝率の計算　勝ち数/(勝ち数+負け数)
-    //report メソッドを呼び出して、勝敗情報を表示してください
-        A1.report();
-        A2.report();
-        A3.report();
-        A4.report();
-        A5.report();
-        A6.report();
+        // 試合勝率の計算 勝ち数/(勝ち数+負け数)
+        // report メソッドを呼び出して、勝敗情報を表示してください
+        a1.report();
+        a2.report();
+        a3.report();
+        a4.report();
+        a5.report();
+        a6.report();
 
     }
 }
-
-
